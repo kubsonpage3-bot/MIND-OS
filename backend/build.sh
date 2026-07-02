c3c340dce58db092b9dee6f40df90f24f3d4f77c
KubsonM <kubsonpage3@gmail.com>
2026-07-02 11:41:43 +0200

    fix: build.sh invoking seed_items incorrectly as management command

--- backend/build.sh
+++ backend/build.sh
@@ -7,6 +7,6 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Seed data and sync boss drops post-migration
-python manage.py seed_items
+python seed_items.py
 python manage.py seed_bosses
 python manage.py sync_boss_drops
